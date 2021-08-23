6c158bf98291cb7f0f30443694d2686618211efb
Luis Alfredo Vejo Mendoza <2205483820101@ingenieria.usac.edu.gt>
2021-08-23 15:22:59 -0600

    Codigo para cambiar sentido de giro en motor DC
    Se agrego el codigo para cambiar el sentido del motor DC mediante botones

--- Tarea2_G4.ino
+++ Tarea2_G4.ino
@@ -1,9 +1,24 @@
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(2,INPUT);
+  pinMode(3,INPUT);
+  pinMode(12,OUTPUT);
+  pinMode(13,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(12,LOW);
+  digitalWrite(13,LOW);
+  if(digitalRead(2) == HIGH){
+    digitalWrite(13,LOW);
+    digitalWrite(12,HIGH);
+  }
+  if(digitalRead(3 == HIGH)){
+    digitalWrite(12,LOW);
+    digitalWrite(13,HIGH);    
+  }
 }
+
+
+  
