054fac45d2463f926dd919dc09818411fbeebad5
Luis Alfredo Vejo Mendoza <2205483820101@ingenieria.usac.edu.gt>
2021-08-25 14:24:24 -0600

    Agregando funciones y arreglando circuito para velocidad de motor

--- Tarea2_G4.ino
+++ Tarea2_G4.ino
@@ -1,12 +1,18 @@
 int pot = A0;
 int valorPot;
+int motorPin = 9;
 
 void setup() {
   // put your setup code here, to run once:
+  //pinMode(motorPin, OUTPUT);
   pinMode(2,INPUT);
   pinMode(3,INPUT);
   pinMode(12,OUTPUT);
   pinMode(13,OUTPUT);
+  /*Serial.begin(9600);
+  while(!Serial);
+  Serial.println("Velocidad de 0 a 255");
+  */
 }
 
 void loop() {
@@ -22,6 +28,14 @@ void loop() {
     digitalWrite(12,LOW);
     digitalWrite(13,HIGH);    
   }
+
+  /*if(Serial.available()){
+    int velocidad = Serial.parseInt();
+    if(velocidad >=0 && velocidad <= 255){
+      analogWrite(motorPin,velocidad);      
+    }
+  }
+  */
 }
 
 
